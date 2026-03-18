739d0f91c6a4ce40b2f4d3ebc237e99a9d6215fe
andrejcvetic <90417276+andrejcvetic@users.noreply.github.com>
2026-03-18 12:04:06 +0000

    Deploying to gh-pages from @ andrejcvetic/andrejcvetic.github.io@25c2e010083855cd845532ead3c5c4085e63f64d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-cv",
           title: "CV",
-          description: "You can download my CV here.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
