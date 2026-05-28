2014db51d46df1dd49433d7780321af7495af7ec
andrejcvetic <90417276+andrejcvetic@users.noreply.github.com>
2026-05-28 12:17:44 +0000

    Deploying to gh-pages from @ andrejcvetic/andrejcvetic.github.io@e48ea9e39d1a1968fd0c3f276e90695304a05ccf 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -63,9 +63,9 @@ ninja.data = [{
           description: "Does state repression impact democratic attitudes of citizens?",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-mental-health",
+          title: 'Mental Health',
+          description: "How political circumstances shape menatal health of citizens?",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
             },},{id: "projects-project-5",
