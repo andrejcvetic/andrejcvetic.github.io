9e396b5dc52a5db40960661ceef88d6bf8e8826c
andrejcvetic <90417276+andrejcvetic@users.noreply.github.com>
2026-05-28 12:12:47 +0000

    Deploying to gh-pages from @ andrejcvetic/andrejcvetic.github.io@89c1c82f6277849550efab8a4c2069a3a0c9dd6f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -58,9 +58,9 @@ ninja.data = [{
           description: "What impacts atttitudes towards asylum seekers?",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-democratic-attitudes-and-state-repression",
+          title: 'Democratic Attitudes and State Repression',
+          description: "Does state repression impact democratic attitudes of citizens?",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
