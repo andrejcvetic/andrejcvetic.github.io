6c9f476adf12da5f6c14a0ccac3c102bf567f974
andrejcvetic <90417276+andrejcvetic@users.noreply.github.com>
2026-05-28 11:58:38 +0000

    Deploying to gh-pages from @ andrejcvetic/andrejcvetic.github.io@f37df5bbb367df7547eff8dcf7669fcdd35178f3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -53,9 +53,9 @@ ninja.data = [{
           description: "Do immigrants react to electoral support for the radical right?",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-asylum-seekers",
+          title: 'Asylum Seekers',
+          description: "What impacts atttitudes towards asylum seekers?",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
